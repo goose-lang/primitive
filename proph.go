89b3ee54b990b0f8e661020d4543f04566075323
Upamanyu Sharma <upamanyu@mit.edu>
2026-01-23 18:53:54 -0500

    Simplify Go implementation of prophecy variables and make them zero-sized

--- proph.go
+++ proph.go
@@ -1,18 +1,9 @@
 package primitive
 
-// Prophecy variables.
-//
-// We represent the name of a prophecy variable via an opaque type. In Go, it
-// does not actually carry any data, but in GooseLang we store the ProphId.
-// However, making a type opaque in Go seems to be tricky. Let's hope adding a
-// private field helps.
-//
-//lint:ignore U1000 p is unused, see above comment.
-type prophId struct{ p struct{} }
-type ProphId = *prophId
+type ProphId struct {}
 
 func NewProph() ProphId {
-	return &prophId{}
+	return ProphId{}
 }
 
 func (p ProphId) ResolveBool(b bool)  {}
